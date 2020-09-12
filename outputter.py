09f086eb452dbf3d7e2db4e5cac31b370c9ed3f1
jialezhang1 <jialezhang1.unimelb.edu.au>
2020-09-12 14:13:46 +0800

    function: Output as csv file added.

--- outputter.py
+++ outputter.py
@@ -1,19 +1,37 @@
-def output_csv(parameters, name):
-	print("Not yet Implemented")
-	
+def output_csv(Result):
+    title = ["Beam Id"]
+    allValues = []
+    for colomn_name in Result["Beams"][0]:
+        title.append(colomn_name)
+
+    i = 0
+    while i + 1 < Result["Number of Beams"]:
+        value = [i + 1]
+        tempdic = Result["Beams"][i]
+        for item in tempdic:
+            value.append(tempdic[item])
+
+        allValues.append(value)
+        i += 1
+
+    test_pd = pd.DataFrame(columns=title, data=allValues)
+    test_pd.to_csv('Documents/Output/Result.csv', encoding='utf-8', index=False)
+
+
 def output_stdout(parameters, name):
-	print("The number of beams: " + str(parameters["Number of Beams"]))
-	for beam in parameters["Beams"]:
-		for p in beam:
-			print(p + " : " + str(beam[p]))
-		print("\n")
-			
+    print("The number of beams: " + str(parameters["Number of Beams"]))
+    for beam in parameters["Beams"]:
+        for p in beamretrieve_parameters:
+            print(p + " : " + str(beam[p]))
+        print("\n")
+
+
 formatter = {
-	'csv': output_csv,
-	'stdout': output_stdout
+    'csv': output_csv,
+    'stdout': output_stdout
 }
 
+
 def output(parameters, name, format):
-	formatter_function = formatter[format]
-	formatter_function(parameters, name)
-	
+    formatter_function = formatter[format]
+    formatter_function(parameters, name)
--- parameter_retrieval.py
+++ parameter_retrieval.py
@@ -2,52 +2,80 @@
 # We are going to get sent files on Monday 7/9 which should help
 # and also we need to code in exceptions where particular values don't exist
 
-#The pydicom library needs to be installed first
+# The pydicom library needs to be installed first
 import pydicom as dicom
+import pandas as pd
+
 
 def retrieve_parameters(filepath):
-	dataset = dicom.read_file(filepath, force=True)
-	parameters = {}
+    dataset = dicom.read_file(filepath, force=True)
+    parameters = {}
 
-	## Number of Beams
-	#print("The number of beams is: " + str(len(dataset.BeamSequence))+"\n")
-	parameters["Number of Beams"] = len(dataset.BeamSequence)
-	parameters["Beams"] = []
-	i=0
-	while i<len(dataset.BeamSequence):
-		if dataset.BeamSequence[i].BeamDescription!="SETUP beam":
-			beam = {}
-			#print("beam #: "+ str(dataset.BeamSequence[i].BeamNumber))
-			beam["Beam Number"] = dataset.BeamSequence[i].BeamNumber
+    ## Number of Beams
+    # print("The number of beams is: " + str(len(dataset.BeamSequence))+"\n")
+    parameters["Number of Beams"] = len(dataset.BeamSequence)
+    parameters["Beams"] = []
+    i = 0
+    while i < len(dataset.BeamSequence):
+        if dataset.BeamSequence[i].BeamDescription != "SETUP beam":
+            beam = {}
+            # print("beam #: "+ str(dataset.BeamSequence[i].BeamNumber))
+            beam["Beam Number"] = dataset.BeamSequence[i].BeamNumber
 
-			#target perscription dose is not complete and is probably wrong
-			#print("The target perscription dose is: "+ str(dataset.DoseReferenceSequence[0].TargetPrescriptionDose))
+            # target perscription dose is not complete and is probably wrong
+            # print("The target perscription dose is: "+ str(dataset.DoseReferenceSequence[0].TargetPrescriptionDose))
 
-			# can't figure out how to find wedge angle
-			# the tag is (300a,00D5) for wedge angle
-			#print("The number of Wedges is: "+ str(dataset.BeamSequence[i].NumberOfWedges))
-			beam["Number of Wedges"] = dataset.BeamSequence[i].NumberOfWedges
+            # can't figure out how to find wedge angle
+            # the tag is (300a,00D5) for wedge angle
+            # print("The number of Wedges is: "+ str(dataset.BeamSequence[i].NumberOfWedges))
+            beam["Number of Wedges"] = dataset.BeamSequence[i].NumberOfWedges
 
-			## GantryAngle 
-			#print("The Gantry Angle is: " + str(dataset.BeamSequence[i].ControlPointSequence[0].GantryAngle))
-			beam["Gantry Angle"] = dataset.BeamSequence[i].ControlPointSequence[0].GantryAngle
+            ## GantryAngle
+            # print("The Gantry Angle is: " + str(dataset.BeamSequence[i].ControlPointSequence[0].GantryAngle))
+            beam["Gantry Angle"] = dataset.BeamSequence[i].ControlPointSequence[0].GantryAngle
 
-			# SSD in millimetres
-			#print("Source to Surface Distance in centimetres: " + str(dataset.BeamSequence[i].ControlPointSequence[0].SourceToSurfaceDistance/10))
-			beam["Source to Surface Distance in centimetres"] = dataset.BeamSequence[i].ControlPointSequence[0].SourceToSurfaceDistance/10
+            # SSD in millimetres
+            # print("Source to Surface Distance in centimetres: " + str(dataset.BeamSequence[i].ControlPointSequence[0].SourceToSurfaceDistance/10))
+            beam["Source to Surface Distance in centimetres"] = dataset.BeamSequence[i].ControlPointSequence[
+                                                                    0].SourceToSurfaceDistance / 10
 
-			# number of fractions
-			#print("The number of fractions planned is: " + str(dataset.FractionGroupSequence[0].NumberOfFractionsPlanned))
-			beam["Number of Fractions Planned"] = dataset.FractionGroupSequence[0].NumberOfFractionsPlanned
+            # number of fractions
+            # print("The number of fractions planned is: " + str(dataset.FractionGroupSequence[0].NumberOfFractionsPlanned))
+            beam["Number of Fractions Planned"] = dataset.FractionGroupSequence[0].NumberOfFractionsPlanned
 
-			#Nominal Beam Energy (only number)
-			#print("The Nominal Beam Energy is: "+ str(dataset.BeamSequence[i].ControlPointSequence[0].NominalBeamEnergy))
-			beam["Nominal Beam Energy"] = dataset.BeamSequence[i].ControlPointSequence[0].NominalBeamEnergy
+            # Nominal Beam Energy (only number)
+            # print("The Nominal Beam Energy is: "+ str(dataset.BeamSequence[i].ControlPointSequence[0].NominalBeamEnergy))
+            beam["Nominal Beam Energy"] = dataset.BeamSequence[i].ControlPointSequence[0].NominalBeamEnergy
 
-			#print("The Monitor Units is: " + str(dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset))
-			beam["Monitor Units"] = dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset
-			
-			parameters["Beams"].append(beam)
-		i+=1
+            # print("The Monitor Units is: " + str(dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset))
+            beam["Monitor Units"] = dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset
 
-	return parameters
+            parameters["Beams"].append(beam)
+        i += 1
+
+    return parameters
+
+
+def output_csv(Result):
+    title = ["Beam Id"]
+    allValues = []
+    for colomn_name in Result["Beams"][0]:
+        title.append(colomn_name)
+
+    i = 0
+    while i + 1 < Result["Number of Beams"]:
+        value = [i + 1]
+        tempdic = Result["Beams"][i]
+        for item in tempdic:
+            value.append(tempdic[item])
+
+        allValues.append(value)
+        i += 1
+
+    test_pd = pd.DataFrame(columns=title, data=allValues)
+    test_pd.to_csv('Documents/Output/Result.csv', encoding='utf-8', index=False)
+
+
+filepath = "Documents/Input/YellowLvlIII_7a.dcm"
+parameters = retrieve_parameters(filepath)
+output_csv(parameters)
