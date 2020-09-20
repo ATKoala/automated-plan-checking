d87159073ea08155e78a742fab29f3480a29c7b5
Raymond Sun <raymonds1@student.unimelb.edu.au>
2020-09-21 01:50:29 +1000

    Replace 0 indexed sequence items with variable
    In the future if it turns out using the first sequence item may cause problems, it will be easier to ctrl-f replace. Otherwise, other random uses of 0 will be affected

--- parameter_retrieval.py
+++ parameter_retrieval.py
@@ -5,6 +5,9 @@ The function to extract parameters from the specified DICOM file.
 # The pydicom library needs to be installed first
 import pydicom as dicom
 
+# TODO investigate whther it's ok to just look for the first item of the sequences
+first_sequence_item = 0
+
 def extract_parameters(filepath):
     dataset = dicom.read_file(filepath, force=True)
     parameters = {}
@@ -23,24 +26,23 @@ def extract_parameters(filepath):
             # print("The number of Wedges is: "+ str(dataset.BeamSequence[i].NumberOfWedges))
             beam["Number of Wedges"] = dataset.BeamSequence[i].NumberOfWedges
 
-            ## Gantry Angle
-            beam["Gantry Angle"] = dataset.BeamSequence[i].ControlPointSequence[0].GantryAngle
+            # Gantry Angle
+            beam["Gantry Angle"] = dataset.BeamSequence[i].ControlPointSequence[first_sequence_item].GantryAngle
 
             # SSD in cm. Originally in millimeters, we convert to cm  
-            beam["Source to Surface Distance in centimetres"] = dataset.BeamSequence[i].ControlPointSequence[
-                                                                    0].SourceToSurfaceDistance / 10
+            beam["Source to Surface Distance in centimetres"] = \
+                dataset.BeamSequence[i].ControlPointSequence[first_sequence_item].SourceToSurfaceDistance / 10
 
-            # Nominal Beam Energy (only number)
-            beam["Nominal Beam Energy"] = dataset.BeamSequence[i].ControlPointSequence[0].NominalBeamEnergy
+            # Nominal Beam Energy (Gy) + Fluence Mode(FFF)
+            beam["Nominal Beam Energy"] = dataset.BeamSequence[i].ControlPointSequence[first_sequence_item].NominalBeamEnergy
+
+            # Total Prescription Dose + Number of fractions
+            beam["Total Prescription Dose"] = dataset.DoseReferenceSequence[first_sequence_item].TargetPrescriptionDose)
+            # Number of fractions - how many fractions the total dose is split up into for treatment
+            beam["Number of Fractions Planned"] = dataset.FractionGroupSequence[first_sequence_item].NumberOfFractionsPlanned
 
             # Monitor Units aka MU aka meterset
             beam["Monitor Units"] = dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset
-
-            # Total Prescription Dose 
-            # TODO investigate whther it's ok to just look for the first item of the sequences
-            beam["Total Prescription Dose"] = dataset.DoseReferenceSequence[0].TargetPrescriptionDose)
-            # Number of fractions - how many fractions the total dose is split up into for treatment
-            beam["Number of Fractions Planned"] = dataset.FractionGroupSequence[0].NumberOfFractionsPlanned
 
             parameters["Beams"].append(beam)
         i += 1
