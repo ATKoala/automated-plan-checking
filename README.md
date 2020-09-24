579140f00409e04ab2825d940fd5c0d1030023e2
jialezhang1 <jialezhang1.unimelb.edu.au>
2020-09-24 13:02:20 +0800

    update_output function added reading dicom bug and "case in range(1,18)" bug fixed Readme updated(more convenient for testing) Some format changing.

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ This repository contains code for extracting and evaluating parameters from radi
 `python PROGRAM_NAME.py -i DICOM_FILE`
 
 Examples:
-    python command_line.py -i "YellowLvlIII_7a.dcm" -c 6(if in same folder, -c means case 6)
-	  python command_line.py -i "C://Users/User/YellowLvlIII_7a.dcm" (if in another folder)
+    python command_line.py -i "YellowLvlIII_7a.dcm" -c 6 -o "D:\COMP90082\automated-plan-checking\Documents\Output\Result.csv" -f "csv"(if in same folder, -c means case 6)
+	  python command_line.py -i "C://Users/User/YellowLvlIII_7a.dcm"(if in another folder)
 ## Testing
 Run the tests with `test command`
 
--- outputter.py
+++ outputter.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 
+
 def output_csv(Result, filepath):
     title = ["Beam Id"]
     allValues = []
@@ -19,26 +20,54 @@ def output_csv(Result, filepath):
         i += 1
 
     test_pd = pd.DataFrame(columns=title, data=allValues)
-    
+
     filepath = filepath + ".csv" if not filepath.endswith(".csv") else filepath
     test_pd.to_csv(filepath, encoding='utf-8', index=False)
     return filepath
 
 
 def output_stdout(parameters, filepath):
-    print("The number of beams: " + str(parameters["Number of Beams"]))
+    for item in parameters:
+        print(item + ": " + parameters[item])
+    '''print("The number of beams: " + str(parameters["Number of Beams"]))
     for beam in parameters["Beams"]:
         for p in beam:
             print(p + " : " + str(beam[p]))
         print("\n")
+    '''
+
+
+def update_output_csv(Result, filepath):
+    title = ["Parameters", "Result"]
+    allValues = []
+    number_of_parameters = len(Result)
+
+    for item in Result:
+        Value = [item, Result[item]]
+        allValues.append(Value)
+
+    test_pd = pd.DataFrame(columns=title, data=allValues)
+
+    filepath = filepath + ".csv" if not filepath.endswith(".csv") else filepath
+    test_pd.to_csv(filepath, encoding='utf-8', index=False)
+    return filepath
+
+
+def update_output_stdout(parameters, filepath):
+    for item in parameters:
+        print(item + ": " + parameters[item])
 
 
 formatter = {
-    'csv': output_csv,
-    'stdout': output_stdout
+    'csv': update_output_csv,
+    'stdout': update_output_stdout
+    # 'csv': output_csv,
+    # 'stdout': output_stdout
 }
 
 
 def output(parameters, filepath, format):
+    # for item in parameters:
+    # print(item+": "+parameters[item])
     formatter_function = formatter[format]
     return formatter_function(parameters, filepath)
