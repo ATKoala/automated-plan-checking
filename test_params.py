51660b9c8c9fa356892b8b1889f1d426a9a2cd2c
Raymond Sun <raymonds1@student.unimelb.edu.au>
2020-09-27 15:50:44 +1000

    Edit comments in test_params.py

--- test_params.py
+++ test_params.py
@@ -1,14 +1,14 @@
-''' Tests for parameters extraction and evaluation
+''' Tests for Parameter Extraction and Evaluation
 
 This module contains a number of tests using python's unittest library.
 See https://docs.python.org/3/library/unittest.html for more information about unittest.
 
-- TestEvaluation is a collection of tests on the parameter evaluation: given a set of parameters, 
-   it verifies that the pass/fail results are as expected
 - TestIMRTExtractionValues is a collection of tests verifying that the correct values are extracted from IMRT file (YellowLvlIII_7a.dcm)
 - TestVMATExtractionValues is a collection of tests verifying that the correct values are extracted from VMAT file (YellowLvlIII_7b.dcm)
+- TestEvaluation is a collection of tests on the parameter evaluation: given a set of parameters, 
+   it verifies that the pass/fail results are as expected
 
-The 2 dicom files are included in the Documents subdirectory.
+The 2 DICOM files tested are included in the Documents subdirectory.
 The correct values for each test are derived from the corresponding pdf reports in each of IMRT and VMAT directories (7a.pdf, 7b.pdf).
 
 Basic method to run all tests: `python -m unittest`
@@ -20,12 +20,11 @@ from parameter_retrieval import extract_parameters, evaluate_parameters
 
 class TestIMRTExtractionValues(unittest.TestCase):
     ''' Tests for verifying the correct values are extracted for IMRT file
-    The 'correct' answers are derived from the vendor report: Documents/Input/7a.pdf
+    The 'correct' answers are derived from the vendor report in Documents/Input/7a.pdf
     '''
     @classmethod
     def setUpClass(self):
-        # We do some set up that is useful across tests in this class 
-        # i.e. running the extraction function once and using the result for each test differently
+        # We use the extraction function once here and inspect the results in the tests below
         self.extracted, _ = extract_parameters('./Documents/Input/YellowLvlIII_7a.dcm')
 
     def test_prescription_dose(self):
@@ -40,14 +39,14 @@ class TestIMRTExtractionValues(unittest.TestCase):
     def test_ssd(self):
         #don't agree with this an an extraction output; 
         # - the pdf has SSDs as 85.19, 89.42, 92.67 89.57, 85.19
-        # - testing against this is no longer in the spirit of using the pdf as a reference
+        # - testing against this is no longer in the 'spirit' of using the pdf as a reference
         self.assertEqual(self.extracted['SSD'], f'?,89,93,89,?')
 
     def test_energy(self):
         self.assertEqual(self.extracted['energy'], 6.0)
 
     def test_wedge_angles(self):
-        # TODO Can't find this in pdf
+        # TODO Can't confirm this result from pdf
         # self.assertEqual(f'{0},{0},{0},{0},{0}',self.extracted['wedge']) 
         pass
 
@@ -57,8 +56,7 @@ class TestVMATExtractionValues(unittest.TestCase):
     '''
     @classmethod
     def setUpClass(self):
-        # We do some set up that is useful across tests in this class 
-        # i.e. running the extraction function once and using the result for each test differently
+        # We use the extraction function once here and inspect the results in the tests below
         self.extracted, _ = extract_parameters('./Documents/Input/YellowLvlIII_7b.dcm')
 
     def test_prescription_dose(self):
@@ -68,11 +66,11 @@ class TestVMATExtractionValues(unittest.TestCase):
         self.assertEqual(self.extracted['collimator'], f'{355}')
 
     def test_gantry_angle(self):
-        #TODO PDF shows 180/360 for a single beam for this one, 
-        # - Naturally this is because of VMAT style of rotating the beam around the patient
-        # - In the dicom, the first ControlPointSequuence item is at 180, 
-        #   does a 360 loop around back to 180, then 360 again back the other way to 180
-        # The only thing is, how are we planning to display this? what value should we show? After we decide I'll update this test case
+        # # TODO PDF shows 180/360 for a single beam for this one, 
+        # # - Naturally this is because of VMAT style of rotating the beam around the patient
+        # # - In the dicom, the first ControlPointSequuence item is at 180, 
+        # #   does a 360 loop around back to 180, then 360 again back the other way to 180
+        # # The only thing is, how are we planning to display this? what value should we show? After we decide I'll update this test case
         # self.assertEqual(f'{180}', self.extracted['gantry']) 
         self.assertEqual(self.extracted['gantry'], 'VMAT File') 
 
@@ -87,10 +85,12 @@ class TestVMATExtractionValues(unittest.TestCase):
         pass
 
 class TestEvaluation(unittest.TestCase):
-    ''' Tests for verifying that parameters are passed and failed correctly
-    The correct answers are derived from the truth table 
+    ''' Tests for verifying that parameter sets are passed correctly
+    Each case (from 1-17) of the truth table has its own test against a set of parameters that *should* pass.
+    It's currently infeasible to test every possible combination of pass/failure values, this is just a basic check. 
+    Also, the correct answers are derived from the truth table. 
 
-    Note that values of None are used for parameters where any value should be accepted (until a better representation is found)
+    Note that values of None are used for parameters where any value should be accepted.
     '''
     @classmethod
     def setUpClass(self):
@@ -111,12 +111,8 @@ class TestEvaluation(unittest.TestCase):
             'meas':'PASS',
             'energy':'PASS'
         }
-        self.maxDiff = None
 
     def test_case_1(self):
-        # passing_parameters is one of a few possible passing sets of parameters
-        # We don't cover every possible success/failure data point because the number of possiblities is too large;
-        #  just a single point for each is used
         case = 1
         passing_parameters = {
             'mode req':'False',
@@ -130,7 +126,7 @@ class TestEvaluation(unittest.TestCase):
             'couch':None,
             'field size':'10x10',
             'wedge':'0',
-            'meas':"'1','3','10','-','-','-','-','-','-'",#TODO disagree with this meas(ure) format
+            'meas':"'1','3','10','-','-','-','-','-','-'",#TODO disagree with this meas(ure) format; shouldn't it be a list?
             'energy':"6,6FFF,10,10FFF,18"
         }
         treatment_type = 'IMRT'
