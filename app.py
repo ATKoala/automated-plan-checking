c238344cdfd8633f8569b6d3defc063bf2f6a971
Raymond Sun <raymonds1@student.unimelb.edu.au>
2020-10-01 17:38:43 +1000

    Simplify output file naming

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ The extractor result is then passed to the outputter.
 import argparse
 from parameter_retrieval import extract_parameters, evaluate_parameters
 from outputter import output
-
+from pathlib import Path
+from sys import exit 
 
 def main():
     # Retrieve user inputs from command line arguments
@@ -15,14 +16,17 @@ def main():
     # Extract parameters from the specified DICOM file
     inputfile = user_input["inputfile"]
     case_number = user_input["case_number"]
-    # print(case_number)
     parameters, file_type = extract_parameters(inputfile)
     evaluations = evaluate_parameters(parameters, case_number, file_type)
 
-    # output the extracted parameters into the format specified by user
-    output_file = user_input["outputfile"] if user_input.get("outputfile", False) else \
-        user_input["inputfile"][0: len(user_input["inputfile"]) - 4]  # remove the ".dcm" file extension
-    output_file = output(evaluations, output_file, user_input["format"])
+    # Decide the name of the output file
+    if user_input["outputfile"]:
+        output_name = user_input["outputfile"]
+    else:
+        output_name = Path(user_input["inputfile"]).stem 
+
+
+    output_file = output(evaluations, output_name, user_input["format"])
     if output_file:
         print("Extracted to file " + output_file)
 
@@ -31,11 +35,11 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Extract a DICOM file")
     parser.add_argument("-i", "--inputfile", required=True,
                         help="The filepath to the input DICOM file.")
-    parser.add_argument("-o", "--outputfile",
+    parser.add_argument("-o", "--outputfile", 
                         help="The filepath to the output file. Default is same as input file.")
     parser.add_argument("-f", "--format", choices=["csv", "stdout"], default="stdout",
                         help="The format of the output file. For now, only CSV or print to standard output are available.")
-    parser.add_argument("-c", "--case_number",
+    parser.add_argument("-c", "--case_number", required=True,
                         help="case number of input number")
     args = parser.parse_args()
     return vars(args)
