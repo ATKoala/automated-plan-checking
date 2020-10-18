711aaa6824dad828e3f02a21ed90f95e74c1ecce
Raymond Sun <raymonds1@student.unimelb.edu.au>
2020-10-18 22:39:08 +1100

    Add empty strings module and top level comment

---
+++ strings.py
@@ -0,0 +1,17 @@
+""" strings Module - A collection of commonly used strings throughout the project
+
+Purpose:
+- Standard string format: e.g. <"10x12" vs "10 x 12"> 
+    Although they both look reasonable, format must be consistent throughout the project for equality comparisons.
+- Easier changes: If a string were to be changed, it can be done with one change in this file.
+
+What kind of strings go in here:
+- Strings that are used in equality comparisons
+- Strings that are used more than 
+
+Including:
+    - Strings from truth table values
+    - Error message strings
+    - Other strings
+"""
+
