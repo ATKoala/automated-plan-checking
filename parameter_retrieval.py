62efb9cc51941303847e26617faadb016d46a26d
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2020-10-19 23:07:18 +1100

    Move the evaluation function for gantry, ssd, and wedge

--- parameter_retrieval.py
+++ parameter_retrieval.py
@@ -35,116 +35,61 @@ def evaluate_parameters(parameter_values, truth_table, case, file_type):
     pass_fail_values = {}
     
     # Check if the case number is valid
-    if case in range(1, 18):
+    if case not in range(1, 18):
+		raise Exception("Invalid case number! Must be between 1 and 18.")
         #print(case)
-        #iterate through each parameter you want to check
-        for param in parameter_values:
-            #print(param)
-            # if the parameter_values[param] has not been extracted we cant determine PASS/FAIL
-            # in these instances we simply return the message to indicate it has not been implemented
-            if parameter_values[param] == strings.NOT_IMPLEMENTED or parameter_values[param] is False:
-                 pass_fail_values[param] = strings.NOT_IMPLEMENTED
-                 
-            # This line checks whether the parameter value found is the same as the truth table value (this is why the formating of the two dictionaries is important) and gives a strings.PASS value
-            # Also there are other instances where a PASS is given such as if the Truth Table is a dash for a given parameter in that case any value will satisfy
-            # Or if the file is a VMAT and the parameter is either a gantry or an SSD
-            # note case-1 is because the first case is 1 but the index position in the list is 0
-            elif param == strings.gantry:
-                if file_type ==strings.VMAT:
-                    pass_fail_values[strings.gantry] = strings.PASS
-                else:
-                    if truth_table[strings.gantry][case - 1] == parameter_values[strings.gantry] or truth_table[strings.gantry][case - 1] == strings.ANY_VALUE:
-                        pass_fail_values[strings.gantry] = strings.PASS
-                    else:
-                        pass_fail_values[strings.gantry] = strings.FAIL
-            elif param == strings.SSD:
-                if file_type ==strings.VMAT:
-                    if truth_table[strings.SSD][case-1] == strings.ANY_VALUE:
-                        pass_fail_values[strings.SSD] = strings.PASS
-                    else:
-                        if truth_table[strings.gantry][case-1] != "-" and truth_table[strings.gantry][case-1] != "error retrieving gantry":
-                            truth_table_gantry_list = truth_table[strings.gantry][case-1].split(',')
-                            truth_table_ssd_list = truth_table[strings.SSD][case-1].split(',')
-                            if len(truth_table_gantry_list) == len(truth_table_ssd_list):
-                                pass_fail_values[strings.SSD] = strings.PASS
-                                i=0
-                                while i < len(truth_table_gantry_list):
-                                    gantry_value = float(truth_table_gantry_list[i])
-                                    ssd_value = truth_table_ssd_list[i]
-                                    if ssd_value != '?':
-                                        ssd_value=float(ssd_value)
-                                        if len(parameter_values[strings.gantry])!=len(parameter_values[strings.SSD]):
-                                               pass_fail_values[strings.SSD] = strings.FAIL
-                                        else:
-                                            j = 0
-                                            while j < len(parameter_values[strings.gantry]):
-                                                if abs(parameter_values[strings.gantry][j] - gantry_value) < 0.3:
-                                                    if abs(parameter_values[strings.SSD][j] - ssd_value) > 1:
-                                                        pass_fail_values[strings.SSD] = strings.FAIL
-                                                j+=1
-                                    i+=1
-                            else:
-                                pass_fail_values[strings.SSD] = strings.FAIL
-                        else:
-                            if truth_table[strings.gantry][case-1] != "-" :
-                                pass_fail_values[strings.SSD] = strings.PASS
-                            else:
-                                pass_fail_values[strings.SSD] = strings.FAIL
-                else:
-                    if truth_table[strings.SSD][case-1] == strings.ANY_VALUE:
-                        pass_fail_values[strings.SSD] = strings.PASS
-                    else:
-                        truth_table_ssd_list = truth_table[strings.SSD][case-1].split(',')
-                        if len(truth_table_ssd_list) == len(parameter_values[strings.SSD]):
-                            pass_fail_values[strings.SSD] = strings.PASS
-                            i=0
-                            while i < len(truth_table_ssd_list):
-                                if truth_table_ssd_list[i] != '?':
-                                    if abs(int(truth_table_ssd_list[i])-float(parameter_values[strings.SSD][i])) > 1:
-                                        pass_fail_values[strings.SSD] = strings.FAIL
-                                i+=1
-                        else:
-                            pass_fail_values[strings.SSD] = strings.FAIL
-            elif param ==strings.wedge:
-                if truth_table[strings.wedge][case - 1] == 'no wedge':
-                    pass_fail_values[strings.wedge] = strings.PASS
-                    for w_angle in parameter_values[strings.wedge].split(","):
-                        if w_angle != 'no wedge':
-                            pass_fail_values[strings.wedge] = strings.FAIL
-                else:
-                    if truth_table[strings.wedge][case - 1] == parameter_values[strings.wedge]:
-                        pass_fail_values[strings.wedge] = strings.PASS
-                    else:
-                        pass_fail_values[strings.wedge] = strings.FAIL
-            elif param == strings.prescription_dose_slash_fractions:
-                pass_fail_values[strings.prescription_dose_slash_fractions]= strings.PASS
-                i=0
-                while i <= 2:
-                    if truth_table[strings.prescription_dose_slash_fractions][case - 1].split("/")[i] != strings.ANY_VALUE and truth_table[strings.prescription_dose_slash_fractions][case - 1].split("/")[i] != parameter_values[strings.prescription_dose_slash_fractions].split("/")[i]:
-                        pass_fail_values[strings.prescription_dose_slash_fractions]= strings.FAIL
-                        break
-                    i+=1
-            elif param == strings.energy:
-                pass_fail_values[param] = strings.NOT_IMPLEMENTED
-            elif param == strings.collimator:
-                if truth_table[param][case - 1] == parameter_values[param] or truth_table[param][
-                    case - 1] == strings.ANY_VALUE:
-                    pass_fail_values[param] = strings.PASS
-                elif truth_table[param][case - 1][0] =='*':
-                    if truth_table[param][case - 1][1:] != parameter_values[param]:
-                        pass_fail_values[param] = strings.PASS
-                    else:
-                        pass_fail_values[param] = strings.FAIL
-                else:            
-                    pass_fail_values[param] = strings.FAIL
-            else:
-                if truth_table[param][case - 1] == parameter_values[param] or truth_table[param][
-                    case - 1] == strings.ANY_VALUE:
-                    pass_fail_values[param] = strings.PASS
-                # if the param has been extracted, it was tested and found to FAIL
-                else:            
-                    pass_fail_values[param] = strings.FAIL
-                
+        
+	context = {
+		parameter_values: parameter_values,
+		truth_table: truth_table,
+		case: case,
+		file_type: file_type
+	}
+	#iterate through each parameter you want to check
+	for param in parameter_values:
+		#print(param)
+		# if the parameter_values[param] has not been extracted we cant determine PASS/FAIL
+		# in these instances we simply return the message to indicate it has not been implemented
+		if parameter_values[param] == NOT_IMPLEMENTED_STRING or parameter_values[param] is False:
+			pass_fail_values[param] = NOT_IMPLEMENTED_STRING
+		else:
+			param_value = parameter_values[param]
+			table_value = truth_table[param][case-1] # note case-1 is because the first case is 1 but the index position in the list is 0
+			context =
+			pass_fail_values[param] = evaluator_functions[param](param_value, table_value, **context)
+			 
+			 
+			 
+			 
+		elif param == strings.prescription_dose_slash_fractions:
+			pass_fail_values[strings.prescription_dose_slash_fractions]= strings.PASS
+			i=0
+			while i <= 2:
+				if truth_table[strings.prescription_dose_slash_fractions][case - 1].split("/")[i] != strings.ANY_VALUE and truth_table[strings.prescription_dose_slash_fractions][case - 1].split("/")[i] != parameter_values[strings.prescription_dose_slash_fractions].split("/")[i]:
+					pass_fail_values[strings.prescription_dose_slash_fractions]= strings.FAIL
+					break
+				i+=1
+		elif param == strings.energy:
+			pass_fail_values[param] = strings.NOT_IMPLEMENTED
+		elif param == strings.collimator:
+			if truth_table[param][case - 1] == parameter_values[param] or truth_table[param][
+				case - 1] == strings.ANY_VALUE:
+				pass_fail_values[param] = strings.PASS
+			elif truth_table[param][case - 1][0] =='*':
+				if truth_table[param][case - 1][1:] != parameter_values[param]:
+					pass_fail_values[param] = strings.PASS
+				else:
+					pass_fail_values[param] = strings.FAIL
+			else:            
+				pass_fail_values[param] = strings.FAIL
+		else:
+			if truth_table[param][case - 1] == parameter_values[param] or truth_table[param][
+				case - 1] == strings.ANY_VALUE:
+				pass_fail_values[param] = strings.PASS
+			# if the param has been extracted, it was tested and found to FAIL
+			else:            
+				pass_fail_values[param] = strings.FAIL
+
     return pass_fail_values
 
 def _extract_file_type(dataset):
@@ -291,3 +236,93 @@ extractor_functions = {
     strings.energy                  : _extract_energy,
 #   'monitor unit'            : # dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset
 }
+
+def _evaluate_gantry(param_value, table_value, **kwargs):
+    # This line checks whether the parameter value found is the same as the truth table value (this is why the formating of the two dictionaries is important) and gives a "PASS" value
+    # Also there are other instances where a PASS is given such as if the Truth Table is a dash for a given parameter in that case any value will satisfy
+    # Or if the file is a VMAT and the parameter is either a gantry or an SSD
+	file_type = kwargs["file_type"]
+    if file_type == strings.VMAT:
+        return strings.PASS
+    else:
+        return strings.PASS if (param_value == table_value or table_value == strings.ANY_VALUE) else strings.FAIL
+
+def _evaluate_ssd(param_value, table_value, **kwargs):
+	if table_value == strings.ANY_VALUE:
+		return strings.PASS
+
+    if kwargs["file_type"] == strings.VMAT:
+		truth_table = kwargs["truth_table"]
+		parameter_values = kwargs["parameter_values"]
+		case = kwargs["case"]
+		
+		if truth_table[strings.gantry][case-1] == strings.ANY_VALUE or parameter_values[strings.gantry] == "error retrieving gantry":
+			if truth_table[strings.gantry][case-1] != strings.ANY_VALUE:
+				pass_fail_values[strings.SSD] = strings.PASS
+			else:
+				pass_fail_values[strings.SSD] = strings.FAIL
+			
+		if len(parameter_values[strings.gantry])!=len(parameter_values[strings.SSD]):
+			return strings.FAIL
+		
+		truth_table_gantry_list = truth_table[strings.gantry][case-1].split(',')
+		truth_table_ssd_list = truth_table[strings.SSD][case-1].split(',')
+		if len(truth_table_gantry_list) != len(truth_table_ssd_list):
+			return strings.FAIL
+		
+		i=0
+		while i < len(truth_table_gantry_list):
+			gantry_value = float(truth_table_gantry_list[i])
+			ssd_value = truth_table_ssd_list[i]
+			if ssd_value == '?':
+				continue
+				
+			ssd_value=float(ssd_value)
+			j = 0
+			while j < len(parameter_values[strings.gantry]):
+				if abs(parameter_values[strings.gantry][j] - gantry_value) < 0.3:
+					if abs(parameter_values[strings.SSD][j] - ssd_value) > 1:
+						return strings.FAIL
+				j+=1
+			i+=1
+		return strings.PASS
+	else:
+		truth_table_ssd_list = table_value.split(',')
+		if len(truth_table_ssd_list) != len(param_value):
+			return strings.FAIL
+		
+		i=0
+		while i < len(truth_table_ssd_list):
+			if truth_table_ssd_list[i] != '?':
+				if abs(int(truth_table_ssd_list[i])-float(parameter_values[strings.SSD][i])) > 1:
+					return strings.FAIL
+			i+=1
+		return strings.PASS
+	
+def _evaluate_wedge(param_value, table_value, file_type, **kwargs):
+	if table_value == 'no wedge':
+		all_no_wedge = all(map(lambda w_angle: w_angle == 'no wedge', param_value.split(',')))
+		return strings.PASS if all_no_wedge else strings.FAIL
+	else:
+		return strings.PASS if table_value == param_value else strings.FAIL
+    
+evaluator_functions = {
+    strings.mode_req               : to_be_implemented, 
+    strings.prescription_dose_slash_fractions    : _extract_prescription_dose, 
+    strings.prescription_point     : to_be_implemented, 
+    strings.isocenter_point        : to_be_implemented,
+        # Isocenter Position TODO:Figuring out what does "SoftTiss" etc means
+        # parameter_values["Isocenter Position"] = dataset.BeamSequence[i].ControlPointSequence[0].IsocenterPosition
+    strings.override             : to_be_implemented, 
+        #I suspect override is at (3008, 0066) tag in the DICOM file but I'm not sure
+    strings.collimator              : _extract_collimator, 
+    strings.gantry                   : _evaluate_gantry, 
+    strings.SSD                        : _evaluate_ssd, 
+    strings.couch                   : to_be_implemented, 
+    strings.field_size              : to_be_implemented,
+    strings.wedge                   : _evaluate_wedge, 
+    strings.meas                    : to_be_implemented, 
+    strings.energy                  : _extract_energy,
+#   'monitor unit'            : # dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset
+}
+        
