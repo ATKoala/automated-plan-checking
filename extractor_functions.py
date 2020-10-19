4d6514adbbd301cc558152d160614fe59f6e0b62
Suryadi Hartanto Tjandra <suryadi.hartanto.tjandra@gmail.com>
2020-10-19 23:36:18 +1100

    Move evaluator functions to a separate file

--- extractor_functions.py
+++ extractor_functions.py
@@ -1,6 +1,5 @@
 # We are mostly using parameters from the first item of Sequences; is this ok?  
 first_sequence_item = 0
-NOT_IMPLEMENTED_STRING = "NOT IMPLEMENTED"
 
 def _extract_prescription_dose(dataset):
     # Total Prescription Dose
@@ -24,7 +23,7 @@ def _extract_prescription_dose(dataset):
     
 def _extract_collimator(dataset):
     #ignore setup beams
-    beams = list(filter(lambda beam: beam.BeamDescription != "SETUP beam", dataset.BeamSequence))
+    beams = list(filter(lambda beam: beam.BeamDescription != strings.SETUP_beam, dataset.BeamSequence))
     # record collimator value in the parameter_values dictionary as a string to be consistant with truth_table format 
     # According to the truth table the collimator only needs to be recorded for cases 1&5 where only 1 beam occurs    
     collimator_value = beams[len(beams)-1].ControlPointSequence[0].BeamLimitingDeviceAngle
@@ -36,45 +35,58 @@ def _extract_gantry(dataset):
         
         #If the dataset is a VMAT file it goes through each of the control point sequence and finds each associated gantry angle and returns the lowest value slash the highest value
         # Also I dont think there is meant to be more than one beam in these cases
-        if file_type == 'VMAT':
+        if file_type == strings.VMAT:
             i = 0
+            vmat_gantry_angles = []
             while i < len(dataset.BeamSequence):
-                if dataset.BeamSequence[i].BeamDescription != "SETUP beam":
-                    min_gantry=str(min([float(control_point.GantryAngle) for control_point in dataset.BeamSequence[i].ControlPointSequence]))
-                    max_gantry=str(max([float(control_point.GantryAngle) for control_point in dataset.BeamSequence[i].ControlPointSequence]))
-                    return min_gantry + '/' + max_gantry
+                if dataset.BeamSequence[i].BeamDescription != strings.SETUP_beam:
+                    for control_point in dataset.BeamSequence[i].ControlPointSequence:
+                        vmat_gantry_angles.append(float(control_point.GantryAngle))
+                    return vmat_gantry_angles
                 i+=1
+            return "error retrieving gantry"
         # If not, then return the Gantry Angle of all beams, separated by commas
         else:
             #ignore setup beams
-            beams = list(filter(lambda beam: beam.BeamDescription != "SETUP beam", dataset.BeamSequence))
+            beams = list(filter(lambda beam: beam.BeamDescription != strings.SETUP_beam, dataset.BeamSequence))
             #obtain the gantry angles of all beams
             gantry_instances = map(lambda beam: str(int(beam.ControlPointSequence[0].GantryAngle)), beams)
             
             return ','.join(gantry_instances)
     except:
-        return '-'
+        return strings.ANY_VALUE
         
 def _extract_ssd(dataset):
 #find SSD in centimeters    
+    file_type = _extract_file_type(dataset)
+    
     ssd_list = []
     try:
-        #ignore setup beams
-        beams = list(filter(lambda beam: beam.BeamDescription != "SETUP beam", dataset.BeamSequence))
-        #obtain the ssd of all beams
-        #in the DICOM file the SSD is given in millimetres so its divided by 10 so its in centimetres
-        ssd_list = list(map(lambda beam: beam.ControlPointSequence[0].SourceToSurfaceDistance / 10, beams))
+        if file_type == strings.VMAT:
+            i = 0
+            vmat_ssd_list = []
+            while i < len(dataset.BeamSequence):
+                if dataset.BeamSequence[i].BeamDescription != strings.SETUP_beam:
+                    for control_point in dataset.BeamSequence[i].ControlPointSequence:
+                        vmat_ssd_list.append(float(control_point.ReferencedDoseReferenceSequence[1].BeamDosePointSSD)/10)
+                    return vmat_ssd_list
+                i+=1
+            return "error retrieving SSD"
+        else:
+            #ignore setup beams
+            beams = list(filter(lambda beam: beam.BeamDescription != strings.SETUP_beam, dataset.BeamSequence))
+            #obtain the ssd of all beams
+            #in the DICOM file the SSD is given in millimetres so its divided by 10 so its in centimetres
+            ssd_list = list(map(lambda beam: beam.ControlPointSequence[0].SourceToSurfaceDistance / 10, beams))
+            return ssd_list
     except:
-        return '-'
-    
-        
-    return ssd_list
+        return "error retrieving SSD"
 
 def _extract_wedge(dataset):
     # It may need more work to deal with VMAT files for cases 6,7,8
     
     #ignore setup beams
-    beams = list(filter(lambda beam: beam.BeamDescription != "SETUP beam", dataset.BeamSequence))
+    beams = list(filter(lambda beam: beam.BeamDescription != strings.SETUP_beam, dataset.BeamSequence))
     # if there are wedges, get the wedge angle of the beam. Otherwise, get 0
     wedge_angles = list(map(lambda beam: str(int(beam.WedgeSequence[0].WedgeAngle)) if int(beam.NumberOfWedges) > 0 else 'no wedge', beams))
     
@@ -86,7 +98,7 @@ def _extract_energy(dataset):
     
     for beam in dataset.BeamSequence:
         #ignore setup beams
-        if beam.BeamDescription == "SETUP beam":
+        if beam.BeamDescription == strings.SETUP_beam:
             continue
         
         #TODO extra LVL3 files given by client are still showing all STANDARD; need to confirm that one of them really 
@@ -96,8 +108,8 @@ def _extract_energy(dataset):
         # Fluence Mode, which may indicate if dose is Flattening Filter Free (but might not! DICOM standard defines it as optional)
         #  -STANDARD     -> not FFF
         #  -NON_STANDARD -> check Fluence Mode ID for a short description of the fluence mode (could be FFF)
-        if beam.PrimaryFluenceModeSequence[first_sequence_item].FluenceMode != 'STANDARD':
-            energy += beam.PrimaryFluenceModeSequence[first_sequence_item].FluenceModeID
+        if beam.PrimaryFluenceModeSequence[first_sequence_item].FluenceMode != strings.STANDARD_FLUENCE:
+            energy += str(beam.PrimaryFluenceModeSequence[first_sequence_item].FluenceModeID)
         
         #energies.append(energy)
     
@@ -106,24 +118,24 @@ def _extract_energy(dataset):
 
 #just a placeholder function to indicate which parameter extractions have not been implemented
 def to_be_implemented(dataset):
-    return NOT_IMPLEMENTED_STRING
+    return strings.NOT_IMPLEMENTED
 
 extractor_functions = {
-    'mode req'                : to_be_implemented, 
-    'prescription dose/#'     : _extract_prescription_dose, 
-    'prescription point'      : to_be_implemented, 
-    'isocentre point'         : to_be_implemented,
+    strings.mode_req                : to_be_implemented, 
+    strings.prescription_dose_slash_fractions     : _extract_prescription_dose, 
+    strings.prescription_point     : to_be_implemented, 
+    strings.isocenter_point        : to_be_implemented,
         # Isocenter Position TODO:Figuring out what does "SoftTiss" etc means
         # parameter_values["Isocenter Position"] = dataset.BeamSequence[i].ControlPointSequence[0].IsocenterPosition
-    'override'                : to_be_implemented, 
+    strings.override                : to_be_implemented, 
         #I suspect override is at (3008, 0066) tag in the DICOM file but I'm not sure
-    'collimator'              : _extract_collimator, 
-    'gantry'                  : _extract_gantry, 
-    'SSD'                     : _extract_ssd, 
-    'couch'                   : to_be_implemented, 
-    'field size'              : to_be_implemented,
-    'wedge'                   : _extract_wedge, 
-    'meas'                    : to_be_implemented, 
-    'energy'                  : _extract_energy,
+    strings.collimator             : _extract_collimator, 
+    strings.gantry                  : _extract_gantry, 
+    strings.SSD                    : _extract_ssd, 
+    strings.couch                   : to_be_implemented, 
+    strings.field_size              : to_be_implemented,
+    strings.wedge                   : _extract_wedge, 
+    strings.meas                    : to_be_implemented, 
+    strings.energy                  : _extract_energy,
 #   'monitor unit'            : # dataset.FractionGroupSequence[0].ReferencedBeamSequence[i].BeamMeterset
 }
